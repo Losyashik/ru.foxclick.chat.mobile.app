8b37fc05740de43febbc5d2dea2ffd1428a0039f
Anton <An.Shkenin@yandex.ru>
2025-11-05 21:17:59 +0300

    feat: edit imports

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import { Link, Tabs } from 'expo-router';
 import React from 'react';
 import { Pressable } from 'react-native';
 
-import { useClientOnlyValue } from '@/components/shared/useClientOnlyValue';
-import { useColorScheme } from '@/components/shared/useColorScheme';
 import Colors from '@/constants/Colors';
+import { useClientOnlyValue } from '@/hooks/useClientOnlyValue';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
